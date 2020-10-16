4b4d5a9955ae80169abea0ff170f2258f498a914
Musa <you@example.com>
2020-10-16 18:45:57 +0330

    2020-10-16-18:45:57 M  webgram/db.py

--- webgram/db.py
+++ webgram/db.py
@@ -6,18 +6,20 @@ if typing.TYPE_CHECKING:
 class Db:
     
     async def set(self: 'webgram.BareServer',key, value):
+        key = str(key)
+        value = str(value)
         try:
-            x = [i async for i in self.master.iter_messages(self.config.CONFIG_CHANNEL, search=key, limit=1) if i.message]
-            if x :
-                await x[0].message.edit(f"{key}:{value}")
-                return True
-            else:
-                await self.client.send_message(self.config.CONFIG_CHANNEL,f"{key}:{value}")
-                return True
+            async for i in self.master.iter_messages(self.config.CONFIG_CHANNEL, search=key, limit=1):
+                if i :
+                    await i.edit(f"{key}:{value}")
+                    return True
+            await self.master.send_message(self.config.CONFIG_CHANNEL,f"{key}:{value}")
+            return True
         except Exception as e: 
             return e
-               
+                
     async def get(self: 'webgram.BareServer',key):
+        key = str(key)
         async for i in self.master.iter_messages(self.config.CONFIG_CHANNEL, search=key, limit=1):
             try:
                 if i :

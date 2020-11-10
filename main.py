8a3aadd3058134bd1856c925d71229728e7c9581
Your Name <you@example.com>
2020-11-10 18:12:58 +0330

    2020-11-10-18:12:58 M  main.py

--- main.py
+++ main.py
@@ -34,41 +34,46 @@ def cancel_tasks() -> None:
         task.cancel()
 
 
-loop = asyncio.get_event_loop()
-    #loop.add_signal_handler(signal.SIGHUP, handle_sighup)
-    #loop.add_signal_handler(signal.SIGTERM, handle_sigterm)
-
-server = webgram.BareServer(loop)
-app = aiohttp.web.Application(client_max_size=1024*1024*20)
-app.add_routes([
-    aiohttp.web.get('/', server.hello),
-    aiohttp.web.get('/m3u/{peer}', server.grab_m3u),
-    aiohttp.web.get('/watch/{peer}/{mid}/{name}', server.watch_stream),
-    aiohttp.web.get('/watch/{hash}', server.watch_stream),
-    aiohttp.web.get('/w/{h}/{name}', server.watch_stream),
-    aiohttp.web.get('/w/{h}', server.watch_stream),
-    aiohttp.web.get('/watch/{hash}/{name}', server.watch_stream),
-    aiohttp.web.get('/test_upload', server.test_upload),
-    aiohttp.web.post('/upload_big', server.upload_big),
-    aiohttp.web.post('/upload', server.upload),
-
-])
+def run():
+	loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGHUP, handle_sighup)
+    loop.add_signal_handler(signal.SIGTERM, handle_sigterm)
+	server = webgram.BareServer(loop)
+	app = aiohttp.web.Application(client_max_size=1024*1024*20)
+	app.add_routes([
+	    aiohttp.web.get('/', server.hello),
+	    aiohttp.web.get('/m3u/{peer}', server.grab_m3u),
+	    aiohttp.web.get('/watch/{peer}/{mid}/{name}', server.watch_stream),
+	    aiohttp.web.get('/watch/{hash}', server.watch_stream),
+	    aiohttp.web.get('/w/{h}/{name}', server.watch_stream),
+	    aiohttp.web.get('/w/{h}', server.watch_stream),
+	    aiohttp.web.get('/watch/{hash}/{name}', server.watch_stream),
+	    aiohttp.web.get('/test_upload', server.test_upload),
+	    aiohttp.web.post('/upload_big', server.upload_big),
+	    aiohttp.web.post('/upload', server.upload),
+	
+	])
+	if __name__ == "__main__":
+		try:
+	        aiohttp.web.run_app(app,host="0.0.0.0", port=server.config.PORT, handle_signals=True)
+	    except ResetException:
+	        logging.warning("Reloading...")
+	        cancel_tasks()
+	        asyncio.set_event_loop(asyncio.new_event_loop())
+	        return True
+	    except GracefulExitException:
+	        logging.warning("Exiting...")
+	        cancel_tasks()
+	        loop.close()
+	return app
+        
+        
 async def main():
     
-    return app
+    return run()
     #aiohttp.web.run_app(app,host="0.0.0.0", port=server.config.PORT)
-    """try:
-        aiohttp.web.run_app(app,host="0.0.0.0", port=server.config.PORT, handle_signals=True)
-    except ResetException:
-        logging.warning("Reloading...")
-        cancel_tasks()
-        asyncio.set_event_loop(asyncio.new_event_loop())
-        return True
-    except GracefulExitException:
-        logging.warning("Exiting...")
-        cancel_tasks()
-        loop.close()"""
+    """"""
 
 
 if __name__ == "__main__":
-    main()
+    run()

c3061b486dba645af2efeee6b48c85561a53a736
Your Name <you@example.com>
2020-11-30 09:33:33 +0330

    2020-11-30-09:33:33 M  bot/webgram/config.py

--- bot/webgram/config.py
+++ bot/webgram/config.py
@@ -17,7 +17,7 @@ class Config:
         
         MASTER_TOKEN = "1BJWap1wBu7oPxOL1j3pBvzwXAbi1hzCDz31rzLYx5KirEII1PYporid59f9Js8Cy_aci_Ru5wq6OY8ignwnIXOOklfoHh1xWz0_PAJ6trNII8t4kvEwRcLDvbrTdf9bH5iWXJxQM_YtQieordW-KcaIL_cxPlK7ge8e1O-MVLfNXpEygB3OCgr9VFYd_uKw0E_uK7nre9C_yMPbOHVRf-qIzIFyyKIE411lHS942DaOL5WhBc-iatRO2MqYi75SJ8awHvJNAVnAiIKFOVOfcNt4JWUB5rqIarV_RDEvVB2QVUZEKHOLd7K8tWVV_wLPt2O9JWGa89wYaFiGDK_Xcoop2TUhrk4E="
         
-        channel = 'UserlandApp'
+        channel = "samtwitter" #'UserlandApp'
 
         STATS_CHANNEL = -1001249461809
         

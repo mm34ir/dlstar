597c86b7aaea24dbcf3ce0054c4da81b1d9526d1
Your Name <you@example.com>
2020-12-11 15:10:27 +0330

    2020-12-11-15:10:27 M  web/webgram/stream_tools.py

--- web/webgram/stream_tools.py
+++ web/webgram/stream_tools.py
@@ -19,15 +19,17 @@ class StreamTools:
     
     @staticmethod
     async def Dl_numbers(message: Union[Message, events.NewMessage.Event]) -> None:
-        if "dl:" not in message.text:
-            await message.edit(f"{message.text}\n dl:1")
-        elif "dl:" in message.text :
+        if " dl" in message.text :
             result = re.sub(
             "dl:(?P<n>\d)",
             lambda m: f"dl:{int(m.groups('n')[0])+1}" ,
             message.text)
             await message.edit(result)
-        
+        else:
+            try:
+                await message.edit(f"{message.text}\n dl:1")
+            except Exception as e:
+                pass
 
     @staticmethod
     def Find(string): 

3fd5e65dd52f0b9ba7ce50100d28f777b8031623
Your Name <you@example.com>
2021-01-04 08:34:24 +0330

    commit 218dbb285c9bf27db271f4d8a1ce5233f76aaf5c Author: Your Name <you@example.com> Date:   Mon Jan 4 08:34:24 2021 +0330
        M  web/main.py
        M  web/webgram/bare.py
    
    diff --git a/web/main.py b/web/main.py
    index 27c1d70..6e90a58 100644
    --- a/web/main.py
    +++ b/web/main.py
    @@ -3,34 +3,26 @@ import webgram
     import aiohttp.web
     import logging
     from concurrent.futures import CancelledError
    -import signal
    
     logging.basicConfig(level=logging.ERROR)
     #logging.basicConfig(level=logging.DEBUG)
    -
    -loop = asyncio.get_event_loop()
    -
    -server = webgram.BareServer(loop)
    -app = aiohttp.web.Application(client_max_size=1024*1024*20)
    -app.add_routes([
    -    aiohttp.web.get('/', server.hello),
    -    aiohttp.web.get('/m3u/{peer}', server.grab_m3u),
    -    aiohttp.web.get('/watch/{peer}/{mid}/{name}', server.watch_stream),
    -    aiohttp.web.get('/watch/{hash}', server.watch_stream),
    -    aiohttp.web.get('/w/{h}/{name}', server.watch_stream),
    -    aiohttp.web.get('/w/{h}', server.watch_stream),
    -    aiohttp.web.get('/watch/{hash}/{name}', server.watch_stream),
    -    aiohttp.web.get('/test_upload', server.test_upload),
    -    aiohttp.web.post('/upload_big', server.upload_big),
    -    aiohttp.web.post('/upload', server.upload),
    -
    -])
    
     async def main():
    +    server = webgram.BareServer()
    +    app = aiohttp.web.Application(client_max_size=1024*1024*20)
    +    app.add_routes([
    +        aiohttp.web.get('/', server.hello),
    +        aiohttp.web.get('/m3u/{peer}', server.grab_m3u),
    +        aiohttp.web.get('/watch/{peer}/{mid}/{name}', server.watch_stream),
    +        aiohttp.web.get('/watch/{hash}', server.watch_stream),
    +        aiohttp.web.get('/w/{h}/{name}', server.watch_stream),
    +        aiohttp.web.get('/w/{h}', server.watch_stream),
    +        aiohttp.web.get('/watch/{hash}/{name}', server.watch_stream),
    +        aiohttp.web.get('/test_upload', server.test_upload),
    +        aiohttp.web.post('/upload_big', server.upload_big),
    +        aiohttp.web.post('/upload', server.upload),
    +
    +    ])
         return app
    
    -
    -if __name__ == "__main__":
    -            aiohttp.web.run_app(app,host="0.0.0.0", port=server.config.PORT, handle_signals=True)
    -
    \ No newline at end of file
    diff --git a/web/webgram/bare.py b/web/webgram/bare.py
    index 03cfb44..7ed1715 100644
    --- a/web/webgram/bare.py
    +++ b/web/webgram/bare.py
    @@ -18,21 +18,20 @@ from telethon.sessions import StringSession
    
     class BareServer(Config, StreamTools, Streamer, Checkers):
    -    client: telethon.TelegramClient
    
    -    def __init__(self, loop: asyncio.AbstractEventLoop):
    +    def __init__(self):
    
             self.client = telethon.TelegramClient(
                 StringSession(), #self.config.SESS_NAME,
                 self.config.APP_ID,
                 self.config.API_HASH,
    -            loop=loop
    +
             ).start(bot_token=self.config.BOT_TOKEN)
    
             self.client2 = telethon.TelegramClient(
                 StringSession(), #self.config.SESS_NAME2,
                 self.config.APP_ID,
                 self.config.API_HASH,
    -            loop=loop
    +
             ).start(bot_token=self.config.BOT_TOKEN2)
    
    \ No newline at end of file

--- web/main.py
+++ web/main.py
@@ -3,34 +3,26 @@ import webgram
 import aiohttp.web
 import logging
 from concurrent.futures import CancelledError
-import signal
 
 logging.basicConfig(level=logging.ERROR)
 #logging.basicConfig(level=logging.DEBUG)
-
-loop = asyncio.get_event_loop()
-
-server = webgram.BareServer(loop)
-app = aiohttp.web.Application(client_max_size=1024*1024*20)
-app.add_routes([
-    aiohttp.web.get('/', server.hello),
-    aiohttp.web.get('/m3u/{peer}', server.grab_m3u),
-    aiohttp.web.get('/watch/{peer}/{mid}/{name}', server.watch_stream),
-    aiohttp.web.get('/watch/{hash}', server.watch_stream),
-    aiohttp.web.get('/w/{h}/{name}', server.watch_stream),
-    aiohttp.web.get('/w/{h}', server.watch_stream),
-    aiohttp.web.get('/watch/{hash}/{name}', server.watch_stream),
-    aiohttp.web.get('/test_upload', server.test_upload),
-    aiohttp.web.post('/upload_big', server.upload_big),
-    aiohttp.web.post('/upload', server.upload),
-
-])
         
         
 async def main():
+    server = webgram.BareServer()
+    app = aiohttp.web.Application(client_max_size=1024*1024*20)
+    app.add_routes([
+        aiohttp.web.get('/', server.hello),
+        aiohttp.web.get('/m3u/{peer}', server.grab_m3u),
+        aiohttp.web.get('/watch/{peer}/{mid}/{name}', server.watch_stream),
+        aiohttp.web.get('/watch/{hash}', server.watch_stream),
+        aiohttp.web.get('/w/{h}/{name}', server.watch_stream),
+        aiohttp.web.get('/w/{h}', server.watch_stream),
+        aiohttp.web.get('/watch/{hash}/{name}', server.watch_stream),
+        aiohttp.web.get('/test_upload', server.test_upload),
+        aiohttp.web.post('/upload_big', server.upload_big),
+        aiohttp.web.post('/upload', server.upload),
+    
+    ])
     return app
 
-
-if __name__ == "__main__":
-            aiohttp.web.run_app(app,host="0.0.0.0", port=server.config.PORT, handle_signals=True)
-            
--- web/webgram/bare.py
+++ web/webgram/bare.py
@@ -18,21 +18,20 @@ from telethon.sessions import StringSession
 
 
 class BareServer(Config, StreamTools, Streamer, Checkers):
-    client: telethon.TelegramClient
     
-    def __init__(self, loop: asyncio.AbstractEventLoop):
+    def __init__(self):
         
         self.client = telethon.TelegramClient(
             StringSession(), #self.config.SESS_NAME,
             self.config.APP_ID,
             self.config.API_HASH,
-            loop=loop
+            
         ).start(bot_token=self.config.BOT_TOKEN)
         
         self.client2 = telethon.TelegramClient(
             StringSession(), #self.config.SESS_NAME2,
             self.config.APP_ID,
             self.config.API_HASH,
-            loop=loop
+            
         ).start(bot_token=self.config.BOT_TOKEN2)
         

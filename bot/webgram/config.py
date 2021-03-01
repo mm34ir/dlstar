ab7ae2120abe5cbc38443161e14468d315e2686f
mm34ir <76752793+mm34ir@users.noreply.github.com>
2021-03-02 00:28:32 +0330

    Update config.py

--- bot/webgram/config.py
+++ bot/webgram/config.py
@@ -3,24 +3,27 @@ import os
 class Config:
 
     class config:
-        APP_ID = 50807
+        APP_ID = 2544613
 
-        API_HASH = "21ab7cb0a453b5e60016dc7bbeb701cb"
+        API_HASH = "0f412888935b3f1eaad6de9d3f4e425c"
         
-        BOT_TOKEN = "1215711283:AAFuct-4Kd1-cDaWp4rLENN08oxgNSOx7FI"
+        BOT_TOKEN = "1600192877:AAGjgAHkG3oaatxH6wh96Ryr8OXuZ3XaJww"
         
-        BOT_TOKEN2 = "1065536817:AAHpPpHQVOGprLOG517xuqDeba4wECwxnUg"
+        BOT_TOKEN2 = "1663037763:AAGc0ORN_qH_y8yWp9VpK3i03ZYrO2Evh_w"
         
-        MASTER_TOKEN = "VLfNXpEygB3OCgr9VFYd_uKw0E_uK7nre9C_yMPbOHVRf-qIzIFyyKIE411lHS942DaOL5WhBc-iatRO2MqYi75SJ8awHvJNAVnAiIKFOVOfcNt4JWUB5rqIarV_RDEvVB2QVUZEKHOLd7K8tWVV_wLPt2O9JWGa89wYaFiGDK_Xcoop2TUhrk4E="
-        
-        channel = 'UserlandApp'
-        
-        STATS_CHANNEL = -1001249461809
+        STATS_CHANNEL = -1001166315615
 
-        ROOT_URI = f"http://dlstarus.dlgram.ml"
+        HOST = "127.0.0.1"
 
-        ROOT_URI_2 = "http://dlstarir.dlgram.ml"
+        PORT = os.getenv('PORT')
+
+        ROOT_URI = f"https://mighty-harbor-72567.herokuapp.com"
+
+        ROOT_URI_2 = "https://mighty-harbor-72567.herokuapp.com"
         
-        ROOT_URI_3 = "http://mmd.dlgram.ml"
+        ROOT_URI_3 = "https://mighty-harbor-72567.herokuapp.com"
 
         ENCODING = "utf8"
+
+
+        # ALLOWED_EXT = ["mkv", "mp4", "flv"]
